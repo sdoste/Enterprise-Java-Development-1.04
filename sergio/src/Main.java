a31fc3defedf0b7af2de6ea51fddc03454fc77a0
sergio <sergiodoste@gmail.com>
2022-07-16 11:09:11 +0200

    -finished

---
+++ sergio/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+public class Main {
+
+    public static void main(String[] args) {
+        int[] testnumbers = new int[]{4, 8, 3, 4, 5, 8};
+        //System.out.println(getDifference(testnumbers));
+        //getTwoSmall(testnumbers);
+        System.out.println(mathExpr(2, 5));
+    }
+
+    public static int getDifference(int[] numbers){
+        int bigN = numbers[0];
+        int smallN = numbers[0];
+        for (int i = 0; i < numbers.length; i++){
+            if (numbers[i] > bigN){
+                bigN = numbers[i];
+            }
+            if (numbers[i] < smallN){
+                smallN = numbers[i];
+            }
+        }
+        return bigN - smallN;
+    }
+    public static void getTwoSmall(int[] numbers){
+        Arrays.sort(numbers);
+        int smallestN = numbers[0];
+        int secondSmallestN = numbers[1];
+        System.out.println(smallestN);
+        System.out.println(secondSmallestN);
+    }
+    public static double mathExpr(int x, int y){
+        double sumN = ((4 * y) / 5) - x;
+        return (x*x) + Math.pow(sumN, 2);
+    }
+}
